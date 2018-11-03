459175cbfb83097293834cae1ee773b206a34a71
yixuanwang <ericxuanwang@gmail.com>
2018-11-03 17:09:11 -0400

    change store.js

--- fullstack-shopping-list-app-with-MERN/client/src/store.js
+++ fullstack-shopping-list-app-with-MERN/client/src/store.js
@@ -18,7 +18,7 @@ const store = createStore(
     initialState,
     compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    
     //this double underscore shit is copied from redux documentation, don't get this wrong
     )
 );
